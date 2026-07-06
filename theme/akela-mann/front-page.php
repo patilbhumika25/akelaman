b7dbf492c991e5f07a900b86e33c4a53070d9dad
Nishant <nishant@akelamann.com>
2026-07-06 12:59:26 +0530

    Remove view details link from services cards

--- theme/akela-mann/front-page.php
+++ theme/akela-mann/front-page.php
@@ -86,7 +86,6 @@
                                 </div>
                                 <h3 class="card-title"><?php echo $title; ?></h3>
                                 <p class="card-desc"><?php echo $desc; ?></p>
-                                <span class="card-link">View details &rarr;</span>
                             </a>
                         </div>
                     <?php endforeach; ?>
