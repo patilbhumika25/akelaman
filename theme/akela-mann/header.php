14f060ff4898c7557aae36cc3342a0a43fa3b4d0
Nishant <nishant@akelamann.com>
2026-07-07 19:35:59 +0530

    Remove Videos from navbar fallback menu

--- theme/akela-mann/header.php
+++ theme/akela-mann/header.php
@@ -31,7 +31,6 @@
                         <li><a href="<?php echo home_url('/#services-section'); ?>">Services</a></li>
                         <li><a href="<?php echo home_url('/#blog-section'); ?>">Blogs</a></li>
 
-                        <li><a href="javascript:void(0)" style="cursor: default;">Videos</a></li>
                         <li><a href="<?php echo home_url('/#site-footer'); ?>">Contact</a></li>
                     </ul>
                 <?php }
